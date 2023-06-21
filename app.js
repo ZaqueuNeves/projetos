21035cf0619d41e1c500921ca86031f6b808100c
ZaqueuNeves <neveszaqueu@gmail.com>
2023-06-20 23:44:03 -0300

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let cliente = {
-    nomeFantasia:["arcos",""],
+    nomeFantasia:[],
     telefone:[]
 }
 
@@ -7,9 +7,28 @@ var p = window.document.getElementsByTagName('p')[0]
 var r = document.getElementsByTagName('p')[1]
 
 
+let n = document.getElementById('nome')
+
+
 function cadastro(){
+
     let n = document.getElementById('nome').value
-    
-    cliente.nomeFantasia.push(n)   
-    console.log(cliente.nomeFantasia)
+    let ler = cliente.nomeFantasia.indexOf(n)
+
+    if (ler >= 0){
+        alert("cliente cadastrado")
+       
     }
+ if (n==""){
+        alert("campo vazio")
+    } else{
+        cliente.nomeFantasia.push(n)   
+        console.log(cliente.nomeFantasia)
+    }
+
+
+ 
+}
+
+
+
