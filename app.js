192d67fad9ea95e2db73ac48e97fa21b433fd92b
ZaqueuNeves <neveszaqueu@gmail.com>
2023-06-21 23:09:12 -0300

    modified:   app.js 	modified:   pedidos.html

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ let n = document.getElementById('nome')
 function cadastro(validar,validarCadastro){
     let n = document.getElementById('nome').value
     if (validar== false){
-        alert("invalido")
+        alert("campo vazio")
     }else if( validarCadastro== false){
    alert("usuario cadastrado")
     }else{ 
-    cliente.nomeFantasia.push(n)   
+    cliente.nomeFantasia.push(n)  
     console.log(cliente.nomeFantasia)}
     }
     
@@ -37,5 +37,17 @@ function validar(){
     }
 
 }
+function limpar(validar){
+    let ler = cliente.nomeFantasia.indexOf(document.getElementById('nome').value)
 
+    if (validar== false){
+        alert("campo vazio")
+    }else if(ler == -1){
+        alert("usuario nao cadastrado")
+    }
+    else{
+        cliente.nomeFantasia.splice(ler,1)
+    } console.log(cliente.nomeFantasia)
+
+}
 
