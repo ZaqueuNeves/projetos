e791e4ed8bf59f3e25f5e546a2bbeac27399e11f
ZaqueuNeves <neveszaqueu@gmail.com>
2023-06-22 22:19:46 -0300

    modified:   app.js 	modified:   pedidos.html

--- app.js
+++ app.js
@@ -8,46 +8,55 @@ var r = document.getElementsByTagName('p')[1]
 
 
 let n = document.getElementById('nome')
-
+let t = document.getElementById('telefone')
 
 function cadastro(validar,validarCadastro){
     let n = document.getElementById('nome').value
+    let t = document.getElementById('telefone').value
+    
     if (validar== false){
         alert("campo vazio")
     }else if( validarCadastro== false){
    alert("usuario cadastrado")
     }else{ 
-    cliente.nomeFantasia.push(n)  
-    console.log(cliente.nomeFantasia)}
+    cliente.nomeFantasia.push(n)
+    cliente.telefone.push(t)  
+    console.log(cliente.nomeFantasia,cliente.telefone)}
     }
     
 
 
 function validarCadastro(){    
     let ler = cliente.nomeFantasia.indexOf(document.getElementById('nome').value)
-        if (ler >= 0){
+    let tel = cliente.telefone.indexOf(document.getElementById('telefone').value)
+        if (ler >= 0 && tel >= 0){
   
             return false
         } 
 
 }
 function validar(){
-    if( document.getElementById('nome').value == ''){
+    if( document.getElementById('nome').value == '' ||
+    document.getElementById('telefone').value== ''){
         return false
     }
 
 }
 function limpar(validar){
     let ler = cliente.nomeFantasia.indexOf(document.getElementById('nome').value)
-
+    let tel = cliente.telefone.indexOf(document.getElementById('telefone').value)
     if (validar== false){
         alert("campo vazio")
-    }else if(ler == -1){
+    }else if(ler == -1 && tel == -1 ){
         alert("usuario nao cadastrado")
+    }else if( ler != tel){
+        alert('invalido, usuario nao apagado')
     }
     else{
+ 
         cliente.nomeFantasia.splice(ler,1)
-    } console.log(cliente.nomeFantasia)
+        cliente.telefone.splice(tel,1)
+    } console.log(cliente.nomeFantasia, cliente.telefone)
 
 }
 
